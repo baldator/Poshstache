6c1b5739a9a43fa266282d7e466b99a6d37948ac
Marco Torello <marcotorello@gmail.com>
2017-07-29 17:47:07 +0200

    Add documentation

--- Poshstache/ConvertTo-PoshstacheTemplate.ps1
+++ Poshstache/ConvertTo-PoshstacheTemplate.ps1
@@ -1,4 +1,20 @@
 function ConvertTo-PoshstacheTemplate{
+    <#
+	.SYNOPSIS
+		Mustache implementation. Mustache is logic-less templates.
+	.DESCRIPTION
+        Convert a template plus an input object into an output file.
+    .PARAMETER InputString
+        A string containing the template
+    .PARAMETER InputFile
+        The path of the file containing the template
+    .PARAMETER ParametersObject
+        A JSON String containing mustache parameters
+    .EXAMPLE
+        ConvertTo-PoshstacheTemplate -InputString "Hi {{name}}!" -ParameterObject @{name:'bob'}
+    .EXAMPLE
+        ConvertTo-PoshstacheTemplate -InputFile .\myInputFile.txt -ParameterObject @{name:'bob'}
+	#>
     param(
         [Parameter(ParameterSetName='String',Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
--- README.md
+++ README.md
@@ -3,7 +3,20 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/gbqt5h9mat4124vr?svg=true)](https://ci.appveyor.com/project/baldator/poshstache)
 
 Module Poshstache is a Powershell implementation of Mustache.
+Mustache is logic-less templates.
 
-# Installation
+This module is based on the .NET Mustache implementation (https://github.com/jdiamond/Nustache)
 
-    Install-Module Poshstache
+
+# Usage
+
+```Powershell
+Install-Module Poshstache
+ConvertTo-PoshstacheTemplate -InputString "Hi {{name}}!" -ParameterObject @{name:'bob'}
+```
+
+# About Mustache
+
+* Mustache webpage: https://mustache.github.io/
+* Mustache demo: https://mustache.github.io/#demo
+* Mustache syntax reference: https://mustache.github.io/mustache.5.html
